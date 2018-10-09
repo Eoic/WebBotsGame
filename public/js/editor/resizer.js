5f68efa25b761a119ead644a85d1169f0cd2f5df
Karolis <karolis.strazdas00@gmail.com>
2018-10-09 15:16:51 +0300

    Add vertical resizing for code editor

---
+++ public/js/editor/resizer.js
@@ -0,0 +1,36 @@
+let isResizing = false;
+let splitter = document.getElementById('splitter');
+
+let editorContainer = document.getElementById('editor');
+let splitterHeight = Number.parseInt(document.defaultView.getComputedStyle(splitter).height);
+
+splitter.addEventListener('mousedown', onMouseDown);
+window.addEventListener('mouseup', onMouseUp);
+window.addEventListener('mousemove', onMouseMove);
+
+// Listener callbacks
+function onMouseDown() {
+    isResizing = true;
+    console.log('Resizing enabled');
+}
+
+function onMouseUp() {
+    isResizing = false;
+    console.log('Resizing disabled');
+    // Save to local storage
+}
+
+function onMouseMove(event) {
+    if (isResizing) {
+        let position = window.innerHeight - event.pageY - splitterHeight / 2;
+
+        if (position < 0)
+            position = 0;
+
+        if (event.pageY < splitterHeight / 2)
+            position = window.innerHeight - splitterHeight;
+
+        splitter.style.bottom = position + 'px'
+        editorContainer.style.height = position + 'px';
+    }
+}
